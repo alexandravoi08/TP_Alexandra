c7d0d5ea2753f55d1d39ce10ec2419c826b9cb43
Alexandra Voisine <alexandravoisine@MacBook-Air-de-Alexandra.local>
2020-03-13 14:36:31 -0400

    lister les parties

--- api.py
+++ api.py
@@ -1,15 +1,21 @@
 import requests
+import json
 
 
-url_base = "https://python.gel.ulaval.ca/quoridor/api"
-
-rep = requests.get("{}/lister/".format(url_base), params={"idul": "alvoi4"})
-
-if rep.status_code == 200:
-    rep = rep.json()
-    print(rep)
-else:
-    print("Le GET sur '{}lister' a produit le code d'erreur {}".format(
-        url_base, rep.status_code)
-    )
-    
+def lister_parties(idul):
+    """
+    Qu'est-ce que fait ma fonction? La fonction permet de lister les parties d'un joueur
+    Quels sont les paramètres en entré de ma fonction? idul
+    Quelle est la sortie et/ou le retour de ma fonction? 
+    """
+    url_lister = "https://python.gel.ulaval.ca/quoridor/api/lister/"
+    reponse = requests.get(url_lister, params={"idul": idul})
+    if reponse.status_code == 200:
+        reponse = reponse.text
+        json_objet = json.loads(reponse)
+        json_format = json.dumps(json_objet, indent=4)
+        print(json_format)
+    else:
+        print("Le GET sur '{}' a produit le code d'erreur {}".format(
+            url_lister, reponse.status_code)
+        )
