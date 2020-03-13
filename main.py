afe87531930748a077da03992db5b075295e561a
Alexandra Voisine <alexandravoisine@MacBook-Air-de-Alexandra.local>
2020-03-13 13:36:51 -0400

    ajuster argparse nouv

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import argparse
 
 
 def analyser_commande():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="Jeu Quoridor - phase 1")
+    parser.add_argument("IDUL", metavar="IDUL", 
+                        default="alvoi4", help="IDUL du joueur")
+    parser.add_argument("-l", "--lister", dest="liste", 
+                       help="Lister les identifiants de vos 20 dernières parties,")
     return parser.parse_args()
 
 
